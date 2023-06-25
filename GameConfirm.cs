bf6ab262a28876ad4ea61ddefab19430da90be73
bhoomikaxxx <102478503+bhoomikaxxx@users.noreply.github.com>
2023-06-25 19:39:42 +0800

    change scene from menu to game

---
+++ GameConfirm.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class GameConfirm : MonoBehaviour
+{
+    public void PlayGameScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
